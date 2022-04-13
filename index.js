abde5eeed9519b2077d39e8a20ef784274bb0dbc
unknown <86469630@qq.com>
2022-04-13 18:32:04 +0800

    tj

--- index.js
+++ index.js
@@ -37,21 +37,16 @@ app.post('/getResult',async (req,res)=> {
   let formData = {
     position:"德悦路375弄"
   }
-  let result = {}
-  await app.post("https://www.vigeotec.com/sh/query",  (formData, res) => {
-    result = httpResponse
-    console.log("httpResponse");
-    console.log(res);
-  })
-  // await httprequest.post({
-  //   url:'https://www.vigeotec.com/sh/query',
-  //   form:formData}, function optionalCallback(err, httpResponse, body) {
-  //    
-  //     result = httpResponse
-  //     console.log('Upload successful!  Server responded with:', body);
-  //   });
-  console.log(req)
-  res.send(result)
+  
+  console.log("222333");
+ 
+    
+    _fn.postData( formData, function(data){
+      console.log("data");
+      console.log(data);
+        res.send(data);
+    });
+  
 });
 
 // 获取计数
@@ -81,3 +76,49 @@ async function bootstrap() {
 }
 
 bootstrap();
+
+_fn = {
+  // getData: function(callback){
+  //     http.get({
+  //         hostname: apiHost,
+  //         path: path
+  //     }, function(res){
+  //         var body = [];
+  //         res.on('data', function(chunk){
+  //             body.push(chunk);
+  //         });
+  //         res.on('end', function(){
+  //             body = Buffer.concat(body);
+  //             callback(body.toString());
+  //         });
+  //     });
+  // },
+  postData: function(data, callback){
+      data = data || {};
+      content = JSON.stringify(data);
+      https://www.vigeotec.com/sh/query
+      var options = {
+          host: "www.vigeotec.com",
+          port: "443",
+          path: "/sh/query",
+          method: 'POST',
+          headers:{
+            'Content-Type': 'multipart/form-data',
+                        'Content-Length': content.length
+            //根据提交请求类型不同而不同，以上适用多媒体文件
+            //可查询各种报头类型代表的意思
+          }
+      };
+      http.request(options, function(res){
+          var _data = '';
+          res.on('data', function(chunk){
+              _data += chunk;
+          });
+          res.on('end', function(){
+              callback(_data);
+          });
+      });
+      req.write(content);
+      req.end()
+  }
+};
